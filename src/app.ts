ad135b1e1d5814f9a2f1fa9ad733c915aa8cab7d
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-11-11 08:37:05 +0900

    nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("hello express\n");
+});
+
+app.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json({ users });
 });
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
+console.log("index")
 app.listen(PORT, () => {
     console.log(`REST API server ready at: http://localhost:${PORT}`);
 });
