ac0eb3fe88520e7c7fa08708cde1be3bed443c4d
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-11-11 08:41:13 +0900

    s users crud

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -10,4 +10,40 @@ router.get("/", async (req: Request, res: Response) => {
     res.json({ users });
 });
 
+// GET /users/:id
+router.get("/:id", async (req: Request, res: Response) => {
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(req.params?.id) },
+    });
+    res.json({ user });
+});
+
+// POST /users
+router.post("/", async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    const user = await prisma.user.create({
+        data: { name, email },
+    });
+    res.json({ user });
+});
+
+// PUT /users/:id
+router.put("/:id", async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    const user = await prisma.user.update({
+        where: { id: parseInt(req.params?.id) },
+        data: { name, email },
+    });
+    res.json({ user });
+});
+
+
+// DELETE /users/:id
+router.delete("/:id", async (req: Request, res: Response) => {
+    const user = await prisma.user.delete({
+        where: { id: parseInt(req.params?.id) },
+    });
+    res.json({ user });
+});
+
 export default router
