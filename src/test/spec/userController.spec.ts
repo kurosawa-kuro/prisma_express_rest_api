e25b19da2000b6266fe8dbc0f3507380424dbf43
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-11-11 18:37:44 +0900

    ｃ

--- src/test/spec/userController.spec.ts
+++ src/test/spec/userController.spec.ts
@@ -1,7 +1,8 @@
-import resetDatabase from "../../utils/resetDatabase";
 import supertest from "supertest";
-import app from "../../app";
 import { PrismaClient } from "@prisma/client";
+
+import app from "../../app";
+import resetDatabase from "../../utils/resetDatabase";
 
 const prisma = new PrismaClient();
 
