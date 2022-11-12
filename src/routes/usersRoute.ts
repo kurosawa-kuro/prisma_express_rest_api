1f64ba8670a265808c4ef13d1c639f08252de377
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-11-12 20:08:23 +0900

    s protect

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,17 +1,20 @@
 import express from 'express';
+import { protect } from '../middleware/authMiddleware';
 import { createUserAction, readUsersAction, readUserAction, updateUserAction, deleteUserAction } from '../controllers/userController';
 
 const router = express.Router();
 
+// readUsersAction,readUserActionはPublicにする
+
 // GET POST
 router.route('/')
-    .get(readUsersAction)
+    .get(protect, readUsersAction)
     .post(createUserAction);
 
 // GET PUT DELETE     
 router.route('/:id')
     .get(readUserAction)
-    .put(updateUserAction)
-    .delete(deleteUserAction);
+    .put(protect, updateUserAction)
+    .delete(protect, deleteUserAction);
 
 export default router;
