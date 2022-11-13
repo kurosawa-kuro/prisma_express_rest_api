8bb2e83a7a5322e056f436f675d7f41ffe925420
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-11-13 16:00:01 +0900

    s frontend 連携

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  type User = {
+    id: number,
+    name: string,
+    email: string,
+  };
+
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    axios.get('/users')
+      .then(res => {
+        setUsers(res.data.users)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        {users && users.map((user) => (
+          <span key={user.id}> {user.email}</span>
+        ))}
       </header>
     </div>
   );
