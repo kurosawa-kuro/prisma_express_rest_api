94eb19036de0797e1e4c27e2c09630988fc9b297
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-11-16 21:04:33 +0900

    s dispatch: AppDispatch

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -4,27 +4,22 @@ import styles from "./Auth.module.css";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { fetchAsyncLogin } from "../features/auth/authSlice";
+import { AppDispatch } from "../app/store";
 
 const Auth = () => {
   const history = useHistory();
-  const dispatch: any = useDispatch();
-  const [username, setUsername] = useState("");
+  const dispatch: AppDispatch = useDispatch();
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(true);
   const [successMsg, setSuccessMsg] = useState("");
 
   const login = async () => {
-    console.log("login")
-    const email = ""
-    const password = ""
-
     const data = { email, password }
-    // const result = await dispatch(fetchAsyncLogin(data))
     const result = await dispatch(
       fetchAsyncLogin(data)
     );
     console.log({ result })
-    // const result = await dispatch()
     // );
     // if (fetchAsyncLogin.fulfilled.match(result)) {
     //   setSuccessMsg("Successfully logged in!");
@@ -40,7 +35,7 @@ const Auth = () => {
       login();
     } else {
       // const result = await dispatch(
-      //   fetchAsyncRegister({ username: username, password: password })
+      //   fetchAsyncRegister({ email: email, password: password })
       // );
       // if (fetchAsyncRegister.fulfilled.match(result)) {
       //   login();
@@ -55,12 +50,12 @@ const Auth = () => {
       <span className={styles.auth__status}>{successMsg}</span>
       <form onSubmit={authUser}>
         <div className={styles.auth__input}>
-          <label data-testid="label-username">Username: </label>
+          <label data-testid="label-email">Username: </label>
           <input
-            data-testid="input-username"
+            data-testid="input-email"
             type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className={styles.auth__input}>
@@ -73,15 +68,16 @@ const Auth = () => {
           />
         </div>
         <button type="submit">{isLogin ? "Login" : "Register"}</button>
-        <div style={{ marginTop: 5 }}>
-          <button>Login/Register</button>
-          {/* <FlipCameraAndroidIcon
+
+      </form>
+      <div style={{ marginTop: 5 }}>
+        <button>Login/Register</button>
+        {/* <FlipCameraAndroidIcon
             data-testid="toggle-icon"
             className={styles.auth__toggle}
             onClick={() => setIsLogin(!isLogin)}
           /> */}
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
