fd22ff4fc9b9ac538cc2a2380aed82b408619dd9
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-11-19 10:22:58 +0900

    s fetchAsyncGetUsers

--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -1,24 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-
+import {
+    fetchAsyncGetUsers, selectUsers
+} from "../features/userSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '../app/store';
 
 const User = () => {
+    const dispatch: AppDispatch = useDispatch();
+    const users: User[] = useSelector(selectUsers);
+
     type User = {
         id: number,
         name: string,
         email: string,
     };
 
-    const [users, setUsers] = useState<User[]>([])
     const [successMsg, setSuccessMsg] = useState("");
 
     useEffect(() => {
-        axios.get('/users')
-            .then(res => {
-                console.log({ res })
-                setUsers(res.data.users)
-            })
-    }, [])
+        const fetchBootLoader = async () => {
+            const result = await dispatch(fetchAsyncGetUsers());
+            if (fetchAsyncGetUsers.rejected.match(result)) {
+                setSuccessMsg("Get error!");
+            }
+        };
+        fetchBootLoader();
+    }, [dispatch]);
 
     return (
         <>
